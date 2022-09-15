14062b2db1a3688cf4ffe6fa389d46c67a4dbf55
Loliconera <69399372+Loliconera@users.noreply.github.com>
2022-09-15 01:45:11 -0500

    Ejercicios tema 8

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Persona persona = new Persona();
+        persona.setEdad(33);
+        int edad = persona.getEdad();
+        persona.setNombre("Gustavo");
+        String nombre = persona.getNombre();
+        persona.setTeléfono(949243737);
+        int teléfono = persona.getTeléfono();
+        System.out.println("El nombre es: " + nombre);
+        System.out.println("tiene " + edad);
+        System.out.println(" y su numero celular es " + teléfono);
+    }
+}
+ class Persona
+ {
+    private int edad;
+    private String nombre;
+    private int teléfono;
+
+     public void setEdad(int Valor)
+     {
+         this.edad = Valor;
+     }
+     public int getEdad()
+     {
+         return this.edad;
+     }
+     public void setNombre(String Valor)
+     {
+         this.nombre = Valor;
+     }
+     public String getNombre()
+     {
+         return this.nombre;
+     }
+     public void setTeléfono(int Valor)
+     {
+         this.teléfono = Valor;
+     }
+     public int getTeléfono()
+     {
+         return this.teléfono;
+     }
+ }
